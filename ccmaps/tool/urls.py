b07c6fa9121e50191e4d9617e9f2b7b902681d5a
Frank Razenberg <frank@zzattack.org>
2013-09-01 13:38:10 +0200

    last fixes for update checker

--- ccmaps/tool/urls.py
+++ ccmaps/tool/urls.py
@@ -1,7 +1,8 @@
 from coffin.conf.urls import *
+from views import *
 
 urlpatterns = patterns('',
 	(r'^$', 'index'),
-	(r'^version_check$', 'version_check$'),
-	(r'^get_latest$', 'get_latest$'),
+	(r'^version_check$', version_check),
+	(r'^get_latest$', get_latest),
 )
--- ccmaps/tool/views.py
+++ ccmaps/tool/views.py
@@ -1,19 +1,25 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.template import RequestContext, Context
+from coffin.shortcuts import render_to_response
+from django.core.mail import send_mail
+from django.core.exceptions import *
+from models import *
+from datetime import datetime
 
-def index(request):
+# Create your views here.
 
 
 def version_check(request):
-    latest_program = ProgramVersion.objects.all().order_by('-version')[0]
+    latest = ProgramVersion.objects.all().order_by('-version')[0]
     template_vars = { 
                      'version': latest.version,
-                     'release_date': datetime.strptime(latest.date, "%Y-%m-%d").date(),
+                     'release_date': latest.release_date,
                      'release_notes': latest.release_notes,
                      'url': latest.file.url,
                      }
     
-    return render_to_response('version_check.xml', template_vars, mimetype="text/xml")
+    return render_to_response('tool/version_check.xml', template_vars, mimetype="text/xml")
 
 def get_latest(request):
-    latest_program = ProgramVersion.objects.all().order_by('-version')[0]
-    return HttpResponseRedirect(latest_program.file.url)
+    latest = ProgramVersion.objects.all().order_by('-version')[0]
+    return HttpResponseRedirect(latest.file.url)
