b60094d5e56590b56fb492294d5ee8522e1d3bc7
Frank Razenberg <frank@zzattack.org>
2013-09-01 13:17:17 +0200

    urls fix

--- ccmaps/urls.py
+++ ccmaps/urls.py
@@ -7,11 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns('',    
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     
-    url(r'^/news', include('ccmaps.news.urls')),
-    url(r'^/maps', include('ccmaps.maps.urls')),
-    url(r'^/discussions', include('ccmaps.discussions.urls')),
-    url(r'^/tool', include('ccmaps.tool.urls')),
+    url(r'^news/', include('ccmaps.news.urls')),
+    url(r'^maps/', include('ccmaps.maps.urls')),
+    url(r'^discussions/', include('ccmaps.discussions.urls')),
+    url(r'^tool/', include('ccmaps.tool.urls')),
     url(r'^', include('ccmaps.home.urls')),
 )
