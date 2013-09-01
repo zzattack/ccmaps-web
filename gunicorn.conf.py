ce5ce1ec989c943aa486cb4288dab929c02f8b6b
Frank <frank@zzattack.org>
2013-09-01 23:06:50 +0200

    version stuff

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import os
 user = "root"
 workers = 2
 bind = "10.31.45.10:8002"
-pidfile = "/tmp/gunicorn-cncmaps.pid"
+pidfile = "/var/run/gunicorn-cncmaps.pid"
 backlog = 2048
 logfile = "/var/log/gunicorn-cncmaps.log"
 loglevel = "debug"
